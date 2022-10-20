b5c9c091670cede36bc296bb9cffdec0bf8d8bbf
pramod0021a <pramod0021a@gmail.com>
2022-10-20 22:40:41 +0530

    first commit

---
+++ src/mains/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'mains'
+
+urlpatterns = [
+    path('', views.index_fun, name='home'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+]
